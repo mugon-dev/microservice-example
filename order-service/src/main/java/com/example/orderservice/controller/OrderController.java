b78f942b63ec599b318cc7e41bb774c148b5d213
jmg <jmg41490400@gmail.com>
2022-09-06 15:15:07 +0900

    add kafka

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -2,13 +2,15 @@ package com.example.orderservice.controller;
 
 import com.example.orderservice.dto.OrderDto;
 import com.example.orderservice.jpa.OrderEntity;
+import com.example.orderservice.messagequeue.KafkaProducer;
+import com.example.orderservice.messagequeue.OrderProducer;
 import com.example.orderservice.service.OrderService;
 import com.example.orderservice.vo.RequestOrder;
 import com.example.orderservice.vo.ResponseOrder;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,24 +18,40 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/order-service")
 @Slf4j
-@RequiredArgsConstructor
 public class OrderController {
     private final Environment env;
     private final OrderService orderService;
 
+    private final KafkaProducer kafkaProducer;
+
+    private final OrderProducer orderProducer;
+
+    @Autowired
+    public OrderController(
+            Environment env,
+            OrderService orderService,
+            KafkaProducer kafkaProducer,
+            OrderProducer orderProducer) {
+        this.env = env;
+        this.orderService = orderService;
+        this.kafkaProducer = kafkaProducer;
+        this.orderProducer = orderProducer;
+    }
+
     @GetMapping("/health_check")
     public String status() {
-        return String.format("It's Working in Order Service on PORT %s",
-                env.getProperty("local.server.port"));
+        return String.format(
+                "It's Working in Order Service on PORT %s", env.getProperty("local.server.port"));
     }
 
     @PostMapping("/{userId}/orders")
-    public ResponseEntity<ResponseOrder> createOrder(@PathVariable("userId") String userId,
-                                                     @RequestBody RequestOrder orderDetails) {
+    public ResponseEntity<ResponseOrder> createOrder(
+            @PathVariable("userId") String userId, @RequestBody RequestOrder orderDetails) {
         log.info("Before add orders data");
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
@@ -41,20 +59,29 @@ public class OrderController {
         OrderDto orderDto = mapper.map(orderDetails, OrderDto.class);
         orderDto.setUserId(userId);
         /* jpa */
-        OrderDto createdOrder = orderService.createOrder(orderDto);
-        ResponseOrder responseOrder = mapper.map(createdOrder, ResponseOrder.class);
+        //    OrderDto createdOrder = orderService.createOrder(orderDto);
+        //    ResponseOrder responseOrder = mapper.map(createdOrder, ResponseOrder.class);
 
+        /* kafka */
+        orderDto.setOrderId(UUID.randomUUID().toString());
+        orderDto.setTotalPrice(orderDetails.getQty() * orderDetails.getUnitPrice());
+
+        /* send this order to the kafka */
+        kafkaProducer.send("example-catalog-topic", orderDto);
+        orderProducer.send("orders", orderDto);
         log.info("After added orders data");
+        ResponseOrder responseOrder = mapper.map(orderDto, ResponseOrder.class);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder);
     }
 
     @GetMapping("/{userId}/orders")
-    public ResponseEntity<List<ResponseOrder>> getOrder(@PathVariable("userId") String userId){
+    public ResponseEntity<List<ResponseOrder>> getOrder(@PathVariable("userId") String userId) {
         Iterable<OrderEntity> orderList = orderService.getOrdersByUserId(userId);
         List<ResponseOrder> result = new ArrayList<>();
-        orderList.forEach(v -> {
-            result.add(new ModelMapper().map(v,ResponseOrder.class));
-        });
+        orderList.forEach(
+                v -> {
+                    result.add(new ModelMapper().map(v, ResponseOrder.class));
+                });
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 }
